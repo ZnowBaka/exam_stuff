925dbd5fb5c1f687087e4d504a00651e335c338f
ZnowBaka <snorrebr@gmail.com>
2026-05-25 16:19:26 +0200

    rust buffer change

--- buffer_overflow/Rust/buffer_rust_experiment/src/main.rs
+++ buffer_overflow/Rust/buffer_rust_experiment/src/main.rs
@@ -1,9 +1,14 @@
-fn main() {
-
-    let mut num = [0; 10];
-    for i in 0 ..11 {
-        num[i] = i;
-        println!("{}", i);
+fn main() {    
+    let mut buffer: [u8; 10] = [0; 10];
+    let overflow = b"this string is to long";
+    println!("Before buffer overflow");
+    println!("string to overflow is {:?}", overflow.len());
+    println!("buffer size is {:?}", buffer.len());
+    for (i, b) in overflow.iter().enumerate(){
+        buffer[i] = *b;
+        println!("{:?}", i);
     }
 
+    println!("after overflow");
+    println!("{:?}", buffer);
 }
