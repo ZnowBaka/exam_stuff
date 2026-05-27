c0b1eee457e83325e8f10bd5063e3f04d5ddc147
ZnowBaka <snorrebr@gmail.com>
2026-05-27 10:55:46 +0200

    buffer loop in C

---
+++ buffer_overflow/C/BufferLoopC.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char buffer[10];
+    char overflow[] = "This string is too long for the buffer";
+    printf("Before overflow:\n");
+    printf("buffer size = %d\n", sizeof(buffer));
+    printf("source string = %s\n", overflow);
+
+    for (int i = 0; overflow[i] != '\0'; i++)
+    {
+        buffer[i] = overflow[i]; // This will write beyond the bounds of 'buffer'
+        printf("Writing '%c' to buffer[%d]\n", overflow[i], i);
+    }
+ 
+    printf("After overflow:\n");
+    printf("buffer contains: %s\n", buffer);
+    return 0;
+}
