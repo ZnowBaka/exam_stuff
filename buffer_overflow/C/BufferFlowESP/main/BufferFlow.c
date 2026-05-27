37366f969b5bbea1071f604a9c2ac8aa4ba0685a
ZnowBaka <snorrebr@gmail.com>
2026-05-27 11:52:49 +0200

    buffer C esp fix

--- buffer_overflow/C/BufferFlowESP/main/BufferFlow.c
+++ buffer_overflow/C/BufferFlowESP/main/BufferFlow.c
@@ -4,14 +4,18 @@
 void app_main(void)
 {
     char buffer[10];
-    const char *overflow = "This string is too long";
-
+    char overflow[] = "This string is too long for the buffer";
     printf("Before overflow:\n");
     printf("buffer size = %d\n", sizeof(buffer));
     printf("source string = %s\n", overflow);
 
-    strcpy(buffer, overflow);
-
+    for (int i = 0; overflow[i] != '\0'; i++)
+    {
+        buffer[i] = overflow[i]; // This will write beyond the bounds of 'buffer'
+        printf("Writing '%c' to buffer[%d]\n", overflow[i], i);
+    }
+ 
     printf("After overflow:\n");
     printf("buffer contains: %s\n", buffer);
+
 }
